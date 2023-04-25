d191cb125f3c567840e211f41a05b6efc11024c3
juani sorato <juanisorato17@yahoo.com>
2023-04-24 22:18:55 -0300

    .

---
+++ 3° Semestre/Python/Leccion04/BD/prueba_bd.py
@@ -0,0 +1,17 @@
+import psycopg2
+
+conexion = psycopg2.connect(
+    user='postgres',
+    password='root',
+    host='127.0.0.1',
+    database='test_bd'
+)
+
+cursor = conexion.cursor()
+sentencia = 'SELECT * FROM persona'
+cursor.execute(sentencia) # De esta manera ejecutamos la sentencia
+registros = cursor.fetchall() # Recuperamos todos los registros que seran una lista
+print(registros)
+
+cursor.close()
+conexion.close()
